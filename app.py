5fb7bcdd3ece99ab286bd366098649448834e412
babakimf <60336573+babakimf@users.noreply.github.com>
2025-07-30 21:07:46 +0330

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, jsonify 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     jobs = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs, company_name = "Jovian")
+    return render_template("home.html", jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
-    return jsonify(jobs)
+    return jsonify(jobs) 
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if job is None:
+        return "Job not found", 404
+    return render_template("jobpage.html", job = job)
--- database.py
+++ database.py
@@ -19,4 +19,15 @@ def load_jobs_from_db():
         jobs = []
         for row in result.all():
             jobs.append(row._asdict())
-        return jobs
+        return jobs 
+    
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), 
+                              {'val':id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:        
+            return rows[0]._asdict()
